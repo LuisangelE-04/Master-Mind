de487e0b0716e5c2f9a0b61e81ffc81c348a0219
LuisangelE-04 <luisangel.esparza2022@gmail.com>
2024-12-06 02:21:00 -0600

    write initial tests for development

--- mastermindapp/mastermind.Tests/MasterMindTests.cs
+++ mastermindapp/mastermind.Tests/MasterMindTests.cs
@@ -1,12 +1,51 @@
 namespace mastermind.Tests;
 
-public class Test {
-    [SetUp]
-    public void Setup() {
-    }
+using static mastermind.Colors;
+using static mastermind.MasterMind;
 
-    [Test]
-    public void Canary() {
-        Assert.Pass();
-    }
+[TestFixture]
+  public class Test {
+  [SetUp]
+  public void Setup() {
+  }
+
+  [Test]
+  public void Canary() {
+    Assert.Pass();
+  }
+
+  [Test]
+  public void GuessWhereAllColorsMatch() {
+    var selectedColors = new List<Colors> { Red, Blue, Green, Yellow, Purple, Orange };
+    var userProvidedColors = selectedColors;
+    
+    var expectedResult = new Dictionary<Match, int> { {Match.EXACT, 6 }, { Match.POSITION, 0 }, {Match.NONE, 0 } };
+    
+    var response = Guess(selectedColors, userProvidedColors);
+
+    CollectionAssert.AreEqual(expectedResult, response);
+  }
+
+  [Test]
+  public void GuessWhereNoColorsMatch() {
+    var selectedColors = new List<Colors> { Red, Blue, Green, Yellow, Purple, Orange };
+    var userProvidedColors = new List<Colors> { Black, Pink, Black, Pink, Black, Pink };
+
+    var expectedResult = new Dictionary<Match, int> { {Match.EXACT, 0 }, { Match.POSITION, 0 }, {Match.NONE, 6 } };
+
+    var response = Guess(selectedColors, userProvidedColors);
+
+    CollectionAssert.AreEqual(expectedResult, response);
+  }
+
+  [Test]
+  public void GuessWhereAllColorsMatchButNotInSamePosition() {
+    var selectedColors = new List<Colors> { Red, Blue, Green, Yellow, Purple, Orange };
+    var userProvidedColors = new List<Colors> { Orange, Purple, Yellow, Green, Blue, Red };
+
+    var expectedResult = new Dictionary<Match, int> { {Match.EXACT, 0 }, { Match.POSITION, 6 }, {Match.NONE, 0 } };
+
+    var response = Guess(selectedColors, userProvidedColors);
+    CollectionAssert.AreEqual(expectedResult, response);
+  }
 }
