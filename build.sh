cc1a08618a1a7d40c1f2638dea662316f1fe2bd0
Luisangel-Macbook <luisangel.esparza2022@gmail.com>
2024-12-07 01:06:05 -0600

    add build file. To be modified

---
+++ build.sh
@@ -0,0 +1,9 @@
+cd mastermindapp
+
+dotnet restore
+
+dotnet build
+
+dotnet run --project mastermind.GUI --framework net8.0-maccatalyst
+
+dotnet run --project mastermind.GUI --framework net8.0-windows10.0.19041.0
