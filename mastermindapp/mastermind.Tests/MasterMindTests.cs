6563bf70b20e30ef96658610f243016c705566a7
LuisangelE-04 <luisangel.esparza2022@gmail.com>
2024-12-06 23:49:27 -0600

    implement initial play tests

--- mastermindapp/mastermind.Tests/MasterMindTests.cs
+++ mastermindapp/mastermind.Tests/MasterMindTests.cs
@@ -1,5 +1,6 @@
 namespace mastermind.Tests;
 
+using static mastermind.Colors;
 using static mastermind.MasterMind;
 
 [TestFixture]
@@ -10,7 +11,41 @@ public class MasterMindTests {
   }
 
   [Test]
-  public void Canary() {
-    Assert.Pass();
+  public void PlayFirstAttemptAllMatches() {
+    var selectedColors = new List<Colors> { Red, Blue, Green, Yellow, Purple, Orange };
+    var userProvidedColors = selectedColors;
+    var numberOfAttempts = 0;
+
+    var expectedResult = (new Dictionary<Match, int> { { Match.EXACT, 6 }, { Match.POSITION, 0 }, { Match.NONE, 0 } }, 1, GameStatus.WON);
+
+    var response = Play(selectedColors, userProvidedColors, numberOfAttempts);
+
+    Assert.AreEqual(expectedResult, response);
+  }
+
+  [Test]
+  public void PlayFirstAttemptNoMatches() {
+    var selectedColors = new List<Colors> { Red, Blue, Green, Yellow, Purple, Orange };
+    var userProvidedColors = new List<Colors> { Black, Pink, Black, Pink, Black, Pink };
+    var numberOfAttempts = 0;
+
+    var expectedResult = (new Dictionary<Match, int> { {Match.EXACT, 0 }, { Match.POSITION, 0 }, { Match.NONE, 6 } }, 1, GameStatus.IN_PROGRESS);
+
+    var response = Play(selectedColors, userProvidedColors, numberOfAttempts);
+
+    Assert.AreEqual(expectedResult, response);
+  }
+
+  [Test]
+  public void PlayFirstAttemptSomeExactAndSomeNonExactMatches() {
+    var selectedColors = new List<Colors> { Red, Blue, Green, Yellow, Purple, Orange };
+    var userProvidedColors = new List<Colors> { Red, Blue, Pink, Yellow, Black, Purple };
+    var numberOfAttempts = 0;
+
+    var expectedResult = (new Dictionary<Match, int> { { Match.EXACT, 3 }, { Match.POSITION, 1 }, { Match.NONE, 2 } }, 1, GameStatus.IN_PROGRESS);
+
+    var response = Play(selectedColors, userProvidedColors, numberOfAttempts);
+
+    Assert.AreEqual(expectedResult, response);
   }
 }
--- mastermindapp/mastermind/MasterMind.cs
+++ mastermindapp/mastermind/MasterMind.cs
@@ -3,7 +3,7 @@
 using static mastermind.GuessEvaluator;
 
 public class MasterMind {
-  public static (Dictionary<Match, int> guess, int attempts,  GameStatus status) Play(IEnumerable<Colors> gameSelectedColors, IEnumerable<Colors> userProvidedColors, int attempts) {
+  public static (Dictionary<Match, int> guess, int attempts, GameStatus status) Play(IEnumerable<Colors> gameSelectedColors, IEnumerable<Colors> userProvidedColors, int attempts) {
     int MAX_ATTEMTPS = 20, MAX_COLORS = 6;
     int currentAttempt = attempts + 1;
 
