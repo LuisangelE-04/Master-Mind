b886941a8a7267b7566d85109cf53d3347446a65
Luisangel-Macbook <luisangel.esparza2022@gmail.com>
2024-12-08 23:53:03 -0600

    add play tests to ensure events are handled corrently

--- mastermindapp/mastermind.Tests/MasterMindTests.cs
+++ mastermindapp/mastermind.Tests/MasterMindTests.cs
@@ -1,5 +1,6 @@
 namespace mastermind.Tests;
 
+using System.Runtime.CompilerServices;
 using static mastermind.Colors;
 using static mastermind.MasterMind;
 
@@ -43,6 +44,71 @@ public class MasterMindTests {
     var numberOfAttempts = 0;
 
     var expectedResult = (new Dictionary<Match, int> { { Match.EXACT, 3 }, { Match.POSITION, 1 }, { Match.NONE, 2 } }, 1, GameStatus.IN_PROGRESS);
+
+    var response = Play(selectedColors, userProvidedColors, numberOfAttempts);
+
+    Assert.AreEqual(expectedResult, response);
+  }
+
+  [Test]
+  public void PlaySecondAttemptExactMatch() {
+    var selectedColors = new List<Colors> { Red, Blue, Green, Yellow, Purple, Orange };
+    var userProvidedColors = selectedColors;
+    var numberOfAttempts = 1;
+
+    var expectedResult = (new Dictionary<Match, int> { { Match.EXACT, 6 }, { Match.POSITION, 0 }, { Match.NONE, 0 } }, 2, GameStatus.WON);
+
+    var response = Play(selectedColors, userProvidedColors, numberOfAttempts);
+
+    Assert.AreEqual(expectedResult, response);
+  }
+
+  [Test]
+  public void PlaySecondAttemptNoMatches() {
+    var selectedColors = new List<Colors> { Red, Blue, Green, Yellow, Purple, Orange };
+    var userProvidedColors = new List<Colors> { Black, Pink, Black, Pink, Black, Pink };
+    var numberOfAttempts = 1;
+
+    var expectedResult = (new Dictionary<Match, int> { { Match.EXACT, 0 }, { Match.POSITION, 0 }, { Match.NONE, 6 } }, 2, GameStatus.IN_PROGRESS);
+
+    var response = Play(selectedColors, userProvidedColors, numberOfAttempts);
+
+    Assert.AreEqual(expectedResult, response);
+  }
+
+  [Test]
+  public void PlayTwentiethAttemptExactMatch() {
+    var selectedColors = new List<Colors> { Red, Blue, Green, Yellow, Purple, Orange };
+    var userProvidedColors = selectedColors;
+    var numberOfAttempts = 19;
+
+    var expectedResult = (new Dictionary<Match, int> { { Match.EXACT, 6 }, { Match.POSITION, 0 }, { Match.NONE, 0 } }, 20, GameStatus.WON);
+
+    var response = Play(selectedColors, userProvidedColors, numberOfAttempts);
+
+    Assert.AreEqual(expectedResult, response);
+  }
+
+  [Test]
+  public void PlayTwentiethAttemptNoMatches() {
+    var selectedColors = new List<Colors> { Red, Blue, Green, Yellow, Purple, Orange };
+    var userProvidedColors = new List<Colors> { Black, Pink, Black, Pink, Black, Pink };
+    var numberOfAttempts = 19;
+
+    var expectedResult = (new Dictionary<Match, int> { { Match.EXACT, 0 }, { Match.POSITION, 0 }, { Match.NONE, 6 } }, 20, GameStatus.LOST);
+
+    var response = Play(selectedColors, userProvidedColors, numberOfAttempts);
+
+    Assert.AreEqual(expectedResult, response);
+  }
+
+  [Test]
+  public void PlayTwentiethAttemptSomeExactAndSomeNonExactMatches() {
+    var selectedColors = new List<Colors> { Red, Blue, Green, Yellow, Purple, Orange };
+    var userProvidedColors = new List<Colors> { Red, Blue, Pink, Yellow, Black, Purple };
+    var numberOfAttempts = 19;
+
+    var expectedResult = (new Dictionary<Match, int> { { Match.EXACT, 3 }, { Match.POSITION, 1 }, { Match.NONE, 2 } }, 20, GameStatus.LOST);
 
     var response = Play(selectedColors, userProvidedColors, numberOfAttempts);
 
